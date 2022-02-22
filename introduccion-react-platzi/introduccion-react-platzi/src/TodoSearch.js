6ca1bb41076652259fdd6f2b67f8767a2543f66e
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-21 21:08:53 -0500

    Studying how the react and javascript state works

--- introduccion-react-platzi/introduccion-react-platzi/src/TodoSearch.js
+++ introduccion-react-platzi/introduccion-react-platzi/src/TodoSearch.js
@@ -2,8 +2,17 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch() {
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    };
+
+
     return (
-        <input className="TodoSearch" placeholder='Search your items ToDo'/>
+        <input 
+            className="TodoSearch" 
+            placeholder='Search your items ToDo'
+            onChange={onSearchValueChange}
+            />
     );
 };
 
