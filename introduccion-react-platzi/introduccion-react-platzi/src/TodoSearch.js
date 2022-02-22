f230a0e35e98ac1ea7c59da46f66de750fddf474
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-22 15:29:07 -0500

    Update TodoSearch.js

--- introduccion-react-platzi/introduccion-react-platzi/src/TodoSearch.js
+++ introduccion-react-platzi/introduccion-react-platzi/src/TodoSearch.js
@@ -2,6 +2,12 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch() {
+    // Para crear movimientos del estado de hace con .useState
+    // Estos son react hooks
+
+    const estado = React.useState();
+    
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
     };
