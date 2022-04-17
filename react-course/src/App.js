6b849641083a1a2f1f47269ea5ddc088fdb21c0e
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-04-17 18:51:37 -0500

    LAYOUT SEMI-COMPLETE

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
+// import './App.css';
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false } ,
+  { text: 'Tomar curso', completed: false } ,
+  { text: 'Renderizar', completed: false } ,
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <TodoCounter />
+
+      <TodoSearch />
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+      
+    </React.Fragment>
   );
 }
 
