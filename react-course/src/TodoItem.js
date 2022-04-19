cdafd9e2fc81f14ea960c42966c6de60cc2465cf
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-04-18 21:38:27 -0500

    Beginning of the hook state react

--- react-course/src/TodoItem.js
+++ react-course/src/TodoItem.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert(`Has completado la tarea de ${props.text}`)
+  };
+  const onDelete = () => {
+    alert(`Has eliminado la tarea de ${props.text}`)
+  };
+
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span 
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={onComplete}
+      >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span
+        className="Icon Icon-delete"
+        onClick={onDelete}
+      >
         X
       </span>
     </li>
