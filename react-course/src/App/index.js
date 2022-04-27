01fea69073b964abb7bcb4466447ea4bbd30d29e
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-04-26 20:18:31 -0500

    Using the localStorage to save the information of the Appq

--- react-course/src/App/index.js
+++ react-course/src/App/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: false } ,
-  { text: 'Tomar curso', completed: false } ,
-  { text: 'Renderizar', completed: false } ,
-  { text: 'Comer', completed: false } ,
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: false } ,
+//   { text: 'Tomar curso', completed: false } ,
+//   { text: 'Renderizar', completed: false } ,
+//   { text: 'Comer', completed: false } ,
+// ];
 
 
 function App() {
@@ -15,8 +15,21 @@ function App() {
   // Hay que llamarlos dentro de los componentes del App
   // useState se usa para principalmente cambiar los valores.
 
+  // Configuración del localStorage
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    // Valor por defecto porque es la primera vez que loguea el User
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    // Si ya se han creado, se va a parsear la información del usuario dentro de un objeto de HS
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
   //Estructura del Search value
-  const [ todos, setTodos ] = React.useState(defaultTodos);
+  const [ todos, setTodos ] = React.useState(parsedTodos);
   const [ searchValue, setSearchValue ] = React.useState('');
   
 
@@ -35,24 +48,30 @@ function App() {
         });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);  
+    saveTodos(newTodos);  
   };
   
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     let newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
     <AppUI 
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
+      totalTodos={totalTodo}
+      completeTodos={completed}
       searchValue={searchValue}
       setSearchValue={setSearchValue}
       searchedTodos={searchedTodos}
